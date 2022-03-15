56677d87997b075fdd09cc930369c75b07cd5788
Yasin Yavuz <yasin_yavuz@live.be>
2022-03-15 13:06:35 +0100

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        darkgrey: "#1F1F1F",
+        grey: "#2E2D2D",
+        bemmblue: "#17CFC4",
+        bemmgreen: "#48EA9C",
+      },
+      fontFamily: {
+        "dm-sans": ["'DM Sans'", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
